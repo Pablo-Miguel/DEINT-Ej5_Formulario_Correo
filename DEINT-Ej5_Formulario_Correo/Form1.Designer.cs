a90c18d99022f826146f96fb0bcadd9b066968ba
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-04-08 13:54:17 +0200

    Agregar archivos de proyecto.

---
+++ DEINT-Ej5_Formulario_Correo/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace DEINT_Ej5_Formulario_Correo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAsunto = new System.Windows.Forms.TextBox();
+            this.btnAdjunto = new System.Windows.Forms.Button();
+            this.txtContenido = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "De:";
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Location = new System.Drawing.Point(123, 37);
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.Size = new System.Drawing.Size(387, 20);
+            this.txtFrom.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(123, 72);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(387, 20);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Contraseña:";
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(123, 108);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(387, 20);
+            this.txtTo.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Para:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Asunto:";
+            // 
+            // txtAsunto
+            // 
+            this.txtAsunto.Location = new System.Drawing.Point(49, 181);
+            this.txtAsunto.Name = "txtAsunto";
+            this.txtAsunto.Size = new System.Drawing.Size(461, 20);
+            this.txtAsunto.TabIndex = 7;
+            // 
+            // btnAdjunto
+            // 
+            this.btnAdjunto.Location = new System.Drawing.Point(123, 143);
+            this.btnAdjunto.Name = "btnAdjunto";
+            this.btnAdjunto.Size = new System.Drawing.Size(387, 23);
+            this.btnAdjunto.TabIndex = 8;
+            this.btnAdjunto.Text = "Archivo adjunto";
+            this.btnAdjunto.UseVisualStyleBackColor = true;
+            this.btnAdjunto.Click += new System.EventHandler(this.btnAdjunto_Click);
+            // 
+            // txtContenido
+            // 
+            this.txtContenido.Location = new System.Drawing.Point(49, 225);
+            this.txtContenido.Multiline = true;
+            this.txtContenido.Name = "txtContenido";
+            this.txtContenido.Size = new System.Drawing.Size(461, 146);
+            this.txtContenido.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(46, 209);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Contenido:";
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(435, 388);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 11;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 434);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtContenido);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnAdjunto);
+            this.Controls.Add(this.txtAsunto);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFrom);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAsunto;
+        private System.Windows.Forms.Button btnAdjunto;
+        private System.Windows.Forms.TextBox txtContenido;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnEnviar;
+    }
+}
+
---
+++ DEINT-Ej5_Formulario_Correo/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DEINT_Ej5_Formulario_Correo
+{
+    public partial class Form1 : Form
+    {
+
+        Attachment data;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            try {
+                MailMessage _Correo = new MailMessage();
+                _Correo.From = new MailAddress(txtFrom.Text);
+
+                _Correo.To.Add(txtTo.Text);
+                _Correo.Subject = txtAsunto.Text;
+                _Correo.Body = txtContenido.Text;
+                if (data != null)
+                {
+                    _Correo.Attachments.Add(data);
+                }
+                _Correo.IsBodyHtml = false;
+                _Correo.Priority = MailPriority.Normal;
+
+                SmtpClient smtp = new SmtpClient();
+                smtp.Credentials = new NetworkCredential(txtFrom.Text, txtPassword.Text);
+
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+
+                smtp.Send(_Correo);
+
+                MessageBox.Show("Correo enviado");
+
+                txtFrom.Clear();
+                txtPassword.Clear();
+                txtTo.Clear();
+                data = null;
+                txtAsunto.Clear();
+                txtContenido.Clear();
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Algo ha fallado al enviar el correo");
+            }
+            
+
+        }
+
+        private void btnAdjunto_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                }
+            }
+
+            MessageBox.Show("Se ha adjuntado el archivo: " + filePath);
+
+            data = new Attachment(filePath, MediaTypeNames.Application.Octet);
+
+        }
+    }
+}
